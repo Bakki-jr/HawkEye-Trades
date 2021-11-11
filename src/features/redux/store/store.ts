fac46703209291923c441c234836a9029362262f
Bakki-jr <bakki.jr@gmail.com>
2021-11-11 17:05:47 +0530

    removed redux-devtools and redux-logger for production environemnt.

--- src/features/redux/store/store.ts
+++ src/features/redux/store/store.ts
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import logger from "redux-logger";
-
+import thunk from "redux-thunk";
 import userReducer from "../slice/user.slice";
 import loginReducer from "../slice/login.slice";
 import signUpReducer from "../slice/sign-up.slice";
@@ -8,21 +8,22 @@ import tradeJournalReducer from "../slice/trade-journal.slice";
 import blogReducer from "../slice/blog.slice";
 import appUsersReducer from "../slice/app-users.slice";
 
-
 const reducer = {
-  signUp: signUpReducer,
-  login: loginReducer,
-  user: userReducer,
-  tradeJournal: tradeJournalReducer,
-  blog: blogReducer,
-  usersInfo: appUsersReducer
+	signUp: signUpReducer,
+	login: loginReducer,
+	user: userReducer,
+	tradeJournal: tradeJournalReducer,
+	blog: blogReducer,
+	usersInfo: appUsersReducer,
 };
 
 export const store = configureStore({
 	reducer,
-	middleware: (getDefaultMiddleware) => getDefaultMiddleware({
-    serializableCheck: false
-  }).concat(logger),
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			serializableCheck: false,
+		}).concat(process.env.NODE_ENV !== "production" ? logger : thunk),
+	devTools: process.env.NODE_ENV !== "production",
 });
 
 export type RootState = ReturnType<typeof store.getState>;
